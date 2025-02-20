bfe309b417c4c0a8d465f8e4e49edb9c907ed317
mkettel <mattkettelkamp@gmail.com>
2025-02-20 13:31:57 -0500

    updated

--- app/page.tsx
+++ app/page.tsx
@@ -2,8 +2,9 @@ import FlowChart from "@/components/FlowChart"
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center p-24">
-      <h1 className="text-4xl font-bold mb-8">Southern Smiles Call Flow</h1>
+    <main className="flex min-h-screen flex-col items-center justify-center p-2 md:p-24">
+      <img className=" mb-6" src="/images/ssmiles-logo.png" alt="logo" />
+      {/* <h1 className="text-4xl text-center font-bold mb-8">Call Flow</h1> */}
       <FlowChart />
     </main>
   )
