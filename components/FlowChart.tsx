200055d1cdee414d677a2147db1a1fbbbef22013
mkettel <mattkettelkamp@gmail.com>
2025-02-28 09:47:56 -0500

    updated

--- components/FlowChart.tsx
+++ components/FlowChart.tsx
@@ -19,6 +19,7 @@ import flowData from "@/data/flow-data.json"
 import { FlowData } from "@/lib/types/flow"
 import ExportCallsButton from "./ExportCallsButton"
 
+// FlowChart component
 export default function FlowChart() {
   const {
     currentStep,
